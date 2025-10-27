a3714defe2387bbabe8cab7847688db0c9c3ddd9
PISEYSRENG <a192@Pisey-Sreng-Macbook.local>
2025-10-27 18:24:36 +0700

    01 Change before deploy

--- API/dist/src/index.js
+++ API/dist/src/index.js
@@ -1,7 +1,7 @@
 import express, { json, urlencoded } from 'express';
 import productsRoutes from './routes/products/index.js';
 import authRoutes from './routes/auth/index.js';
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(urlencoded({ extended: false }));
 app.use(json());
