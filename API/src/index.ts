1daabdc51bff0f5b4b59eb6d02c5b199c74b4b82
PISEYSRENG <a192@Pisey-Sreng-Macbook.local>
2025-10-27 16:45:45 +0700

    Another Changes before Deploy

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,9 +1,8 @@
 import express, { json, urlencoded } from 'express';
 import productsRoutes from './routes/products/index.js';
 import authRoutes from './routes/auth/index.js';
-import serverless from 'serverless-http';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(urlencoded({ extended: false }));
 app.use(json());
@@ -16,10 +15,6 @@ app.get('/', (req, res) => {
 app.use('/products', productsRoutes);
 app.use('/auth', authRoutes);
 
-if (process.env.NODE_ENV === "dev") {
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-    })
-}
-
-export const handler = serverless(app);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
