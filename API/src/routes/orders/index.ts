b709a0105265a6f1dc369b38f1d66dea3b7d8e8d
PISEYSRENG <a192@Pisey-Sreng-Macbook.local>
2025-10-28 11:12:51 +0700

    Orders CRUD

--- API/src/routes/orders/index.ts
+++ API/src/routes/orders/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createOrder } from "./ordersController.js";
+import { createOrder ,listOrders,getOrder, updateOrder} from "./ordersController.js";
 import { validateData } from "../../middlewares/validationMiddleware.js";
-import { insertOrderWithItemsSchema } from "../../db/ordersSchema.js";
+import { insertOrderWithItemsSchema, updateOrderSchema } from "../../db/ordersSchema.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
 const router = Router();
 
+router.get('/',verifyToken, listOrders);
+router.get('/:id',verifyToken, getOrder);
+router.put('/:id',verifyToken, validateData(updateOrderSchema), updateOrder);
 router.post('/',verifyToken , validateData(insertOrderWithItemsSchema) ,createOrder);
 
 export default router;
--- API/src/routes/orders/ordersController.ts
+++ API/src/routes/orders/ordersController.ts
@@ -1,24 +1,67 @@
 import { Request, Response } from "express";
 import { db } from '../../db/index.js';
 import { orderItemsTable, ordersTable } from "../../db/ordersSchema.js";
+import {eq} from 'drizzle-orm';
 
-export async function createOrder (req: Request, res: Response){
+export async function createOrder(req: Request, res: Response) {
     try {
         const userId = req.userId;
         const items = req.body.items;
         console.log(userId);
         if (!userId) {
-            res.status(400).json({message: 'No User Id'});    
+            res.status(400).json({ message: 'No User Id' });
         }
-        const [order] = await db.insert(ordersTable).values({userId: Number(userId)}).returning();
+        const [order] = await db.insert(ordersTable).values({ userId: Number(userId) }).returning();
 
         const orderItems = items.map((item: any) => ({
             ...item,
             orderId: order.id,
         }));
         const newOrderItems = await db.insert(orderItemsTable).values(orderItems).returning();
-        res.status(201).json({...order, items: newOrderItems})
+        res.status(201).json({ ...order, items: newOrderItems })
     } catch (error) {
-        res.status(400).json({message: 'Invalid Oder Data'});
+        res.status(400).json({ message: 'Invalid Oder Data' });
+    }
+};
+
+export async function listOrders(req: Request, res: Response) {
+    try {
+        const orders = await db.select().from(ordersTable);
+        res.json(orders);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+export async function getOrder (req: Request, res: Response) {
+    try {
+        const id = parseInt(req.params.id);
+        const orderWithItems = await db.select().from(ordersTable).where(eq(ordersTable.id, id)).leftJoin(orderItemsTable,eq(ordersTable.id , orderItemsTable.orderId));
+        if (orderWithItems.length === 0) {
+            res.status(404).send('Order not found');
+        }
+        const mergedOrder = {
+            ...orderWithItems[0].orders,
+            items: orderWithItems.map((oi) => oi.order_items),
+        };
+
+        res.status(200).json(mergedOrder);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+export async function updateOrder (req: Request, res: Response) {
+    try {
+        const id = parseInt(req.params.id);
+        const [updatedOrder] = await db.update(ordersTable).set(req.body).where(eq(ordersTable.id, id)).returning();
+        if (!updatedOrder) {
+            res.status(404).send('Order not found');
+        } else {
+            res.status(202).json(updateOrder);
+        }
+    } catch (error) {
+        res.status(500).send(error);
     }
 };
