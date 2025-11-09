59e47824753b02367d51298111b6d2074a48802a
PISEYSRENG <a192@Pisey-Sreng-Macbook.local>
2025-11-09 18:35:09 +0700

    02 Stripe

--- API/src/routes/stripe/stripeController.ts
+++ API/src/routes/stripe/stripeController.ts
@@ -1,9 +1,50 @@
 import { Request, Response } from "express";
+import Stripe from 'stripe';
 
-export async function getKeys(req: Request, res: Response) {    
-    res.json({publishableKey: process.env.STRIPE_PUBLISABLE_KEY});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function getKeys(req: Request, res: Response) {
+    res.json({ publishableKey: process.env.STRIPE_PUBLISABLE_KEY });
 }
 
-export async function createPaymentIntent (req: Request, res: Response){
-    res.json({message: 'Hello Stripe Intense Payment'});
+export async function createPaymentIntent(req: Request, res: Response) {
+    // TODO: Add info about the Customer
+    const customer = await stripe.customers.create();
+    const customerSession = await stripe.customerSessions.create({
+        customer: customer.id,
+        components: {
+            mobile_payment_element: {
+                enabled: true,
+                features: {
+                    payment_method_save: 'enabled',
+                    payment_method_redisplay: 'enabled',
+                    payment_method_remove: 'enabled'
+                }
+            },
+        },
+    });
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        // {apiVersion: '2024-09-30.acacia'}
+    );
+
+    //  TODO Calculate the amount dynamically
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: 'eur',
+        customer: customer.id,
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.json({
+        paymentIntent: paymentIntent.client_secret,
+        ephemeralKey: ephemeralKey.secret,
+        customerSessionClientSecret: customerSession.client_secret,
+        customer: customer.id,
+        publishableKey: process.env.STRIPE_PUBLISABLE_KEY,
+    });
 }
