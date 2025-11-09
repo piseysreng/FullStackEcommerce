c35b927dd9355217bc830ecfbb0551cb4e46a547
PISEYSRENG <a192@Pisey-Sreng-Macbook.local>
2025-11-09 15:53:54 +0700

    Fixed the Eslint Config

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,12 +1,21 @@
 import { withGluestackUI } from '@gluestack/ui-next-adapter';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   transpilePackages: [],
 };
 export default withGluestackUI(nextConfig);
+// import { withGluestackUI } from '@gluestack/ui-next-adapter';
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   transpilePackages: [],
+// };
+// export default withGluestackUI(nextConfig);
